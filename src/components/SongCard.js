94a43458a4a026708c3db339020f8c208ee5b211
Sheena Sang <sheenasany@gmail.com>
2022-08-22 18:27:16 -0400

    add toggle for lyrics button

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SongCard({ song }) {
+
+    const [lyricsToggle, setLyricsToggle] = useState(true)
+    const handleLyricsToggle = () => {
+        setLyricsToggle(!lyricsToggle)
+    }
 
     return (
         <div>
             <img src={song.albumImage} alt="album"/>
             <h3>{song.title}</h3>
             <h4>{song.artist}</h4>
-            <button>Lyrics 🎶</button>
+            <button onClick={handleLyricsToggle}>{lyricsToggle ? "Show Lyrics 🎶" : "Hide Lyrics 🎧"}</button>
+            {lyricsToggle ? null: song.lyrics}
             <button>Add to Favorites 💖</button>
             
 
