1847ee7ea3e03faa16621a1b80f06f68a83e2733
Sheena Sang <sheenasany@gmail.com>
2022-08-22 12:23:05 -0400

    added content and css to about and nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,16 @@ import React, { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <nav>
-            <Link className={"default-link"} to="/">About</Link>
-            <Link className={"default-link"} to="/songs">Songs</Link>
-            <Link className={"default-link"} to="/favorite-songs">Favorite Songs</Link>
+        <nav> 
+            <span>
+            <Link to="/">About</Link>
+            <Link to="/songs">Songs</Link>
+            <Link to="/favorite-songs">Favorite Songs</Link>
+            </span>
         </nav>
     )
 }
 
 export default NavBar;
+
+// className="default-link"
