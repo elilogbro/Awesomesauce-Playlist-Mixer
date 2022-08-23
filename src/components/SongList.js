6742c17a4b19dac490172002ebdd9c535906eda6
Eli <+>
2022-08-23 09:46:50 -0400

    Added form, changed CSS to desired layout

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SongCard from "./SongCard"
+import Form from "./Form";
 
 function SongList({ songs, filterGenre }) {
     const songList = songs.map(song => <SongCard key={song.id} song={song} />)
@@ -8,15 +9,18 @@ function SongList({ songs, filterGenre }) {
         <div className="songs-container">
             <h2>Choose a Genre</h2>
             <div className="genre-navbar">
-                <nav onClick={filterGenre}>Rock</nav>
-                <nav onClick={filterGenre}>Folk</nav>
-                <nav onClick={filterGenre}>Indie</nav>
-                <nav onClick={filterGenre}>Hip-Hop</nav>
-                <nav onClick={filterGenre}>Electronic</nav>
+                    <nav onClick={filterGenre}>Rock</nav>
+                    <nav onClick={filterGenre}>Folk</nav>
+                    <nav onClick={filterGenre}>Indie</nav>
+                    <nav onClick={filterGenre}>Hip-Hop</nav>
+                    <nav onClick={filterGenre}>Electronic</nav>
             </div>
-            <div className="songlist-container">
-                <h1>AwesomeSauce Song List</h1>
-                {songList}
+            <div className="form-songlist-container">
+                <Form />
+                <div className="songlist-container">
+                    <h1>AwesomeSauce Song List</h1>
+                    {songList}
+                </div>
             </div>
         </div>
     )
