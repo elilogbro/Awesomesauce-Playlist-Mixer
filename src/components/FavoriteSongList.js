748cb18d50793867a67ac0eab40932772de953f7
Eli <+>
2022-08-24 11:28:34 -0400

    Added conditional rendering to SongCard

--- src/components/FavoriteSongList.js
+++ src/components/FavoriteSongList.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import SongCard from './SongCard'
 
-function FavoriteSongList({favorite, handleDeletedSong, handleFavorites}) {
+function FavoriteSongList({favorite, setFavorite}) {
 
     document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkNTBAb-8-L9kCYaSSqdJRbDM2NTNTFMK1gg&usqp=CAU')"
 
+
+    const handleDeletedFavorite = (id) => {
+        setFavorite(favorite.filter(song => id !== song.id))
+    }
+
+    const handleFavorites = () => {
+        console.log('from favorites')
+    }
+
     const renderFavorites = favorite.map(song => {
-        return (<SongCard key={song.id} song={song} handleDeletedSong={handleDeletedSong} handleFavorites={handleFavorites}/>)
+        return (<SongCard  key={song.id} song={song} isInFavorites={true} handleDelete={handleDeletedFavorite} handleFavorites={handleFavorites}/>)
     })
 
     return(
--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,18 +1,11 @@
 import React, { useState } from 'react';
 
-function SongCard({ song, handleDeletedSong, handleFavorites }) {
+function SongCard({ song, handleDelete, isInFavorites, handleFavorites}) {
 
     const [lyricsToggle, setLyricsToggle] = useState(true)
     
     const handleLyricsToggle = () => {
         setLyricsToggle(!lyricsToggle)
-    }
-
-    const handleDelete = () => {
-        fetch(`http://localhost:3000/songs/${song.id}`, {
-            method: "DELETE"
-        })
-            handleDeletedSong(song.id)
     }
 
     return (
@@ -23,7 +16,8 @@ function SongCard({ song, handleDeletedSong, handleFavorites }) {
             <img src={song.albumImage} alt="album"/>
             <h3>{song.title}</h3>
             <h4>{song.artist}</h4>
-            <button className="rainbow-button" onClick={() => handleFavorites(song)}>Add to Favorites 💖</button>
+            { !isInFavorites && <button className="rainbow-button" onClick={() => handleFavorites(song)}>Add to Favorites 💖</button> }
+            { isInFavorites && <p>{song.isOnTour ? "Artist is on tour!" : "Artist is not on tour!"} </p>}
             <button className="rainbow-button" onClick={handleLyricsToggle}>{lyricsToggle ? "Show Lyrics 🎶" : "Hide Lyrics 🎧"}</button>
             {lyricsToggle ? null: song.lyrics}
             <button className="rainbow-button" onClick={() => handleDelete(song.id)}>Delete ❌</button>
