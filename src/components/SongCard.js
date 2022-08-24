46c80fa927e791cf7063243a9ad66d0850f7b14d
Eli <+>
2022-08-24 12:18:47 -0400

    Added state for the buttons/song details

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -3,9 +3,14 @@ import React, { useState } from 'react';
 function SongCard({ song, handleDelete, isInFavorites, handleFavorites}) {
 
     const [lyricsToggle, setLyricsToggle] = useState(true)
+    const [details, setDetails] = useState(false)
     
     const handleLyricsToggle = () => {
         setLyricsToggle(!lyricsToggle)
+    }
+
+    const showDetails = () => {
+        setDetails(!details)
     }
 
     return (
@@ -13,14 +18,18 @@ function SongCard({ song, handleDelete, isInFavorites, handleFavorites}) {
              <style>
                 @import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');
             </style>
-            <img src={song.albumImage} alt="album"/>
-            <h3>{song.title}</h3>
-            <h4>{song.artist}</h4>
-            { !isInFavorites && <button className="rainbow-button" onClick={() => handleFavorites(song)}>Add to Favorites 💖</button> }
-            { isInFavorites && <p>{song.isOnTour ? "Artist is on tour!" : "Artist is not on tour!"} </p>}
-            <button className="rainbow-button" onClick={handleLyricsToggle}>{lyricsToggle ? "Show Lyrics 🎶" : "Hide Lyrics 🎧"}</button>
-            {lyricsToggle ? null: song.lyrics}
-            <button className="rainbow-button" onClick={() => handleDelete(song.id)}>Delete ❌</button>
+            <img onClick={showDetails} src={song.albumImage} alt="album"/>
+            { details ?
+                <div className="songcard-container">
+                <h3>{song.title}</h3>
+                <h4>{song.artist}</h4>
+                { !isInFavorites && <button className="rainbow-button" onClick={() => handleFavorites(song)}>Add to Favorites 💖</button> }
+                { isInFavorites && <p>{song.isOnTour ? "Artist is on tour!" : "Artist is not on tour!"} </p>}
+                <button className="rainbow-button" onClick={handleLyricsToggle}>{lyricsToggle ? "Show Lyrics 🎶" : "Hide Lyrics 🎧"}</button>
+                {lyricsToggle ? null: song.lyrics}
+                <button className="rainbow-button" onClick={() => handleDelete(song.id)}>Delete ❌</button>
+            </div>
+            : null}
           
         </div>
     )
